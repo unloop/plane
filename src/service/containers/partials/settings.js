3b80327bfd18f507bc9e3e1edb52ebf4417c19ee
unloop <pastor.konstantin@gmail.com>
2017-05-22 21:50:41 +0300

    update service settings page

--- src/service/containers/partials/settings.js
+++ src/service/containers/partials/settings.js
@@ -39,11 +39,30 @@ const NamespaceSettingsContainer = (props) => {
     props.dispatch(serviceActions.remove.RemoveActionCreators(service));
   }
 
+
+  let hook_url = [process.env.REACT_APP_API_HOST, "hook", props.service.meta.hook].join("/");
+
   return (
     <div className="container-fluid">
 
       <div className="settings-block-item">
         <ServiceGeneralForm updateHandler={updateGeneralInfoHandler} {...props} />
+      </div>
+
+      <Divider/>
+
+      <div className="settings-block-item">
+        <div className="row">
+
+          <div className="col-md-4 col-xs-12">
+            <h3>Webhooks</h3>
+            <desc>Allows external services to notify system that service update required</desc>
+          </div>
+
+          <div className="col-md-8 col-xs-12">
+            <pre style={{marginTop: "25px"}}>{hook_url}</pre>
+          </div>
+        </div>
       </div>
 
       <Divider/>
@@ -58,7 +77,8 @@ const NamespaceSettingsContainer = (props) => {
         <ServiceDeleteForm removeHandler={removeHandler} {...props} />
       </div>
     </div>
-  );
+  )
+    ;
 };
 
 const mapStateToProps = (state, props) => {
